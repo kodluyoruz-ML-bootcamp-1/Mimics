6430506944e6e8e3a1aaaf867dd5eb572b84bee2
Berk Sudan <berk.sudan.96@gmail.com>
2019-10-04 19:18:49 +0300

    camera implementation is improved but not completed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from classifier import Classifier
-from data_preparer import PreProcessor, DatasetBuilder
-from facial_landmarks import LandMarker
+from image_classifier import Classifier
+# from data_preparer import PreProcessor, DatasetBuilder
+from data_land_marker import LandMarker
 
 IMAGES_DIR = 'data/images/'
 PREDICTOR_PATH = 'shape_predictor_68_face_landmarks.dat'
@@ -19,40 +19,16 @@ def main():
     """
     # Pre-process data
     PreProcessor(data_dir=IMAGES_DIR).preprocess()
+    """
 
+    """
     # Build dataset as csv
     dataset_builder = DatasetBuilder(data_dir=IMAGES_DIR, class_feature='emotion', land_marker=land_marker)
     dataset_builder.build(target_csv=INITIAL_CSV, write_header=True)
     """
 
     rf_classifier = Classifier(csv_path=FINAL_CSV, algorithm='RandomForest', land_marker=land_marker)
-    rf_classifier.classify('img.png', 'img2.png')
-
-    exit(96)
-
-    imgs = []
-
-    shape_list = []
-    for i in shape_list:
-        print(i)
-
-    list1 = shape_list[0]
-    list2 = shape_list[1]
-
-    img1 = imgs[0]
-    img2 = imgs[1]
-
-    point_dists = []
-    for point_list1, point_list2 in zip(list1, list2):
-        point_dist = ((point_list2[0] - point_list1[0]) ** 2 + (point_list2[1] - point_list1[1]) ** 2) ** 0.5
-        point_dists.append(point_dist)
-
-    final_dist = sum(map(lambda x: x ** 2, point_dists)) ** 0.5
-
-    print(point_dists)
-    print('IMAGES: ', img1, img2)
-    print(final_dist)
-    exit(95)
+    print(rf_classifier.classify('smiling_berk.png'))# 'img.png', 'img2.png'))
 
 
 if __name__ == "__main__":
