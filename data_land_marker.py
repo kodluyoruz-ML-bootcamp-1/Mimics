3a94c42d3b2c9a8945915352b696c4719c8e2ecb
bs <23002942+berksudan@users.noreply.github.com>
2019-10-05 04:33:22 +0300

    finalized for demo

--- data_land_marker.py
+++ data_land_marker.py
@@ -1,138 +1,49 @@
 # import the necessary packages
+
+
 import cv2.cv2 as cv2
 import dlib
-import imutils
 import numpy as np
-import math
-# construct the argument parser and parse the arguments
-from imutils import face_utils
 
+import data_transformer as dt
 
-def convert_landmarks_to_vectors(x_list: [], y_list: []):
-    landmarks_vectorised = []
-
-    x_mean = np.mean(x_list)
-    y_mean = np.mean(y_list)
-    x_central = [(x - x_mean) for x in x_list]
-    y_central = [(y - y_mean) for y in y_list]
-
-    for x, y, w, z in zip(x_central, y_central, x_list, y_list):
-        landmarks_vectorised.append(w)
-        landmarks_vectorised.append(z)
-        meannp = np.asarray((y_mean, x_mean))
-        coornp = np.asarray((z, w))
-        dist = np.linalg.norm(coornp - meannp)
-        landmarks_vectorised.append(dist)
-        landmarks_vectorised.append((math.atan2(y, x) * 360) / (2 * math.pi))
-    return landmarks_vectorised
+LAND_MARK_POINTS_SIZE = 68
+CLAHE_CLIP_LIMIT = 2.0
+TILE_GRID_SIZE = (8, 8)
 
 
 class LandMarker:
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    clahe = cv2.createCLAHE(clipLimit=CLAHE_CLIP_LIMIT, tileGridSize=TILE_GRID_SIZE)
 
     def __init__(self, landmark_predictor_path: str):
-        # initialize dlib's face detector (HOG-based) and then create
-        # the facial landmark predictor
-        self.predictor = dlib.shape_predictor(landmark_predictor_path)
-        self.detector = dlib.get_frontal_face_detector()
+        self.predictor = dlib.shape_predictor(landmark_predictor_path)  # the facial landmark predictor
+        self.detector = dlib.get_frontal_face_detector()  # initialize dlib's face detector
 
-    def load_img(self, img_path: str) -> np.ndarray:
-        # load the input image, resize it, and convert it to gray-scale
-        image = cv2.imread(img_path)  # Read image
-        gray_scaled_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # convert to gray-scale
-        return self.clahe.apply(gray_scaled_img)
-
-    def img_to_landmarks(self, img_path: str) -> tuple:
-        img = self.load_img(img_path=img_path)
-
-        # detect faces in the grayscale image
+    def img_to_landmarks(self, img):
         detections = self.detector(img, 1)
-        assert len(detections) == 1, 'Error: length of detections = %d, expected = 1' % len(detections)
-
+        if len(detections) == 0:
+            return None
         for k, d in enumerate(detections):  # For all detected face instances individually
             shape = self.predictor(img, d)  # Draw Facial Landmarks with the predictor class
-            x_list = [float(shape.part(i).x) for i in range(1, 68)]
-            y_list = [float(shape.part(i).y) for i in range(1, 68)]
+            x_list = tuple(float(shape.part(i).x) for i in range(1, LAND_MARK_POINTS_SIZE))
+            y_list = tuple(float(shape.part(i).y) for i in range(1, LAND_MARK_POINTS_SIZE))
 
-            return tuple(convert_landmarks_to_vectors(x_list, y_list))
+            return tuple(dt.points_to_vectors(x_list, y_list))
 
-    def img_to_landmarks_legacy(self, img_path: str):
-        # load the input image, resize it, and convert it to grayscale
-        image = cv2.imread(img_path)
-        image = imutils.resize(image, width=500)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    def img_to_landmark_points(self, img: np.ndarray):
+        rectangles = self.detector(img, 1)
+        for (i, rect) in enumerate(rectangles):
+            shape = self.predictor(img, rect)
+            return dt.shape_to_np(shape, size=LAND_MARK_POINTS_SIZE)
 
-        # detect faces in the grayscale image
-        rects = self.detector(gray, 1)
 
-        shape = None
-        # loop over the face detections
-        for (i, rect) in enumerate(rects):
-            # determine the facial landmarks for the face region, then
-            # convert the facial landmark (x, y)-coordinates to a NumPy
-            # array
-            shape = self.predictor(gray, rect)
+    def img_to_rectangle(self, img: np.ndarray):
+        detections = self.detector(img, 1)
+        if len(detections) == 0:
+            return 0, 0, 0, 0
 
-            data_points = []
-            for idx in range(0, shape.num_parts):
-                data_points.append(shape.part(idx).x)
-                data_points.append(shape.part(idx).y)
+        for (i, rect) in enumerate(detections):  # loop over the face detections
+            # convert dlib's rectangle to a OpenCV-style bounding box, i.e. (x, y, w, h)
+            (x, y, w, h) = dt.rectangular_to_bounding_box(rect)
 
-            shape_np = self.shape_to_np(shape)
-            print([shape_np])
-            print(data_points)
-            exit(99)
-            # print(shape_np)
-
-            """
-            # convert dlib's rectangle to a OpenCV-style bounding box
-            # [i.e., (x, y, w, h)], then draw the face bounding box
-            (x, y, w, h) = self.rect_to_bb(rect)
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    
-            # show the face number
-            cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-    
-            print(cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
-                              cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2))
-    
-            # loop over the (x, y)-coordinates for the facial landmarks
-            # and draw them on the image
-            for (x, y) in shape_np:
-                cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
-    
-            cv2.imshow("Output", image)
-            # cv2.imwrite(IMAGE.replace('.jpg', '_landmarked.bmp'), image)
-            cv2.waitKey(0)
-            
-            """
-
-            return data_points  # Assumed that number of face in picture = 1
-
-    @staticmethod
-    def rect_to_bb(rect):
-        # take a bounding predicted by dlib and convert it
-        # to the format (x, y, w, h) as we would normally do
-        # with OpenCV
-        x = rect.left()
-        y = rect.top()
-        w = rect.right() - x
-        h = rect.bottom() - y
-
-        # return a tuple of (x, y, w, h)
-        return x, y, w, h
-
-    @staticmethod
-    def shape_to_np(shape, dtype="int"):
-        # initialize the list of (x, y)-coordinates
-        # coords = np.zeros((68, 2), dtype=dtype)
-        coords = []
-
-        # loop over the 68 facial landmarks and convert them
-        # to a 2-tuple of (x, y)-coordinates
-        for i in range(0, 68):
-            coords.append((shape.part(i).x, shape.part(i).y))
-
-        # return the list of (x, y)-coordinates
-        return coords
+            return x, y, w, h  # Assumed that number of face in picture = 1
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from image_classifier import Classifier
-# from data_preparer import PreProcessor, DatasetBuilder
 from data_land_marker import LandMarker
+
+from image_classifier import Classifier
+from camera_classifier import CameraClassifier
 
 IMAGES_DIR = 'data/images/'
 PREDICTOR_PATH = 'shape_predictor_68_face_landmarks.dat'
@@ -9,26 +10,21 @@ INITIAL_CSV = 'data/csvs/ds_original.csv'
 FINAL_CSV = 'data/csvs/ds_classes_equalized.csv'
 
 
-def shape_to_coordinates(img_shape):
-    return ','.join(['(%s-%s)' % (i[0], i[1]) for i in img_shape])
-
-
 def main():
     land_marker = LandMarker(landmark_predictor_path=PREDICTOR_PATH)
+    rf_classifier = Classifier(csv_path=FINAL_CSV, algorithm='SVM', land_marker=land_marker)
 
     """
+    from data_preparer import PreProcessor, DatasetBuilder
+    
     # Pre-process data
-    PreProcessor(data_dir=IMAGES_DIR).preprocess()
-    """
+    PreProcessor(data_dir=IMAGES_DIR).preprocess() 
 
-    """
     # Build dataset as csv
-    dataset_builder = DatasetBuilder(data_dir=IMAGES_DIR, class_feature='emotion', land_marker=land_marker)
-    dataset_builder.build(target_csv=INITIAL_CSV, write_header=True)
+    DatasetBuilder(data_dir=IMAGES_DIR, class_feature='emotion', landmarker=landmarker).build(target=INITIAL_CSV)
     """
 
-    rf_classifier = Classifier(csv_path=FINAL_CSV, algorithm='RandomForest', land_marker=land_marker)
-    print(rf_classifier.classify('smiling_berk.png'))# 'img.png', 'img2.png'))
+    CameraClassifier(classifier_model=rf_classifier).execute()
 
 
 if __name__ == "__main__":
